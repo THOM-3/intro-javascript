d62d77387f2588167c764a75b0915f5386a16143
THOM-3 <71737896+THOM-3@users.noreply.github.com>
2023-03-02 20:51:01 -0300

    Aula 0203

---
+++ exemplo/003-estrutra-condicional.js
@@ -0,0 +1,24 @@
+let entrada = require('prompt-sync')();
+
+let idade = entrada('Digite sua idade: ');
+
+if (idade == 18) {
+    console.log('Você pode se matricular na auto-escola!');
+}
+
+if (idade > 18) {
+    console.log('Você tem idade para dirigir caso tenha habilitação.');
+}
+
+if (idade < 18) {
+    console.log('Você não tem idade para dirigir!');
+}
+
+console.log('A idade digitadsa é: ' + idade);
+
+
+if (idade >= 18) {
+    console.log('Maior');
+}else{
+    console.log('Menor');
+}
---
+++ exemplo/004-atividade-if.js
@@ -0,0 +1,10 @@
+// Crie um progama que receba duas notas de 0 a 10,
+// Calcule a média e com a média
+// exiba as seguintes mensagens para para cada faixa de valores
+// nota < 5 = I, nota >= 5 e < 6.5 = R, nota > 6.5 e < 8.5 = B
+// e nota >= 8.5 MB
+
+let entrada = entrada('prompt-sync')();
+
+let media = entrada('Digite sua nota');
+if (media ) 
