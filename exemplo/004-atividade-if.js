51bd56a5de6db4291ec60d4d4763db44d31089fb
THOM-3 <71737896+THOM-3@users.noreply.github.com>
2023-03-09 19:41:02 -0300

    Update 004-atividade-if.js

--- exemplo/004-atividade-if.js
+++ exemplo/004-atividade-if.js
@@ -4,7 +4,22 @@
 // nota < 5 = I, nota >= 5 e < 6.5 = R, nota > 6.5 e < 8.5 = B
 // e nota >= 8.5 MB
 
-let entrada = entrada('prompt-sync')();
 
-let media = entrada('Digite sua nota');
-if (media ) 
+let entrada = require('prompt-sync')();
+
+let nota1 = parseFloat(entrada("Digite a primeira nota (de 0 a 10):"));
+let nota2 = parseFloat(entrada("Digite a segunda nota (de 0 a 10):"));
+
+// Calcula a média
+let media = (nota1 + nota2) / 2;
+
+// Exibe a mensagem correspondente à média
+if (media < 5) {
+    console.log("I");
+} else if (media >= 5 && media < 6.5) {
+    console.log("R");
+} else if (media >= 6.5 && media < 8.5) {
+    console.log("B");
+} else {
+    console.log("MB");
+}
